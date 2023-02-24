bb6eb4d80e07b5d339d898ff33de105df59e8d9d
Aman Sirohi <amansirohi077@gmail.com>
2023-02-25 02:24:50 +0530

    contact

--- send_email.php
+++ send_email.php
@@ -1,4 +1,4 @@
-
+<!-- 
 <?php
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
@@ -9,20 +9,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$phone = $_POST["phone"];
 	$message = $_POST["message"];
 
-	$to = "amansirohi@duck.com"; // replace with your own email address
+	$to = "amansirohi@duck.com"; 
 	$headers = "From: $email\r\n" .
 	           "Reply-To: $email\r\n" .
 	           "X-Mailer: PHP/" . phpversion();
 	$body = "Name: $name\nEmail: $email\nMobile: $phone\n\n$message";
 
 	// Send email using Gmail SMTP server
-	require_once "PHPMailer/src/PHPMailer.php"; // path to PHPMailer autoload file
+	require_once "PHPMailer/src/PHPMailer.php"; 
 	$mail = new PHPMailer\PHPMailer\PHPMailer(true);
 	$mail->isSMTP();
-    $mail->Host = 'smtp.porkbun.com';
+    $mail->Host = 'smtp.sendgrid.net';
     $mail->SMTPAuth = true;
-    $mail->Username = ''; // replace with your Gmail email address
-    $mail->Password = ''; // replace with your Gmail password
+    $mail->Username = 'apikey';
+    $mail->Password = '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
+	'; 
     $mail->SMTPSecure = 'ssl';
     $mail->Port = 465;
 	
@@ -37,4 +38,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	    echo "Message sent successfully!";
 	}
 }
-?> 
+?>  -->
+<?php
+
+require_once('PHPMailer/src/PHPMailer.php'); // Include SendGrid library
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$mobile = $_POST['phone'];
+$message = $_POST['message'];
+
+$email_body = "This is a message from the Contact Us form:\n\nName: $name\nEmail: $email\nMobile: $phone\nMessage: $message";
+
+$email = new \SendGrid\Mail\Mail(); // Create SendGrid object
+
+$email->setFrom("$email", "$name"); // Set sender email and name
+$email->setSubject("Contact Us Form Submission"); // Set email subject
+$email->addTo("amansirohi@duck.com", "Recipient Name"); // Set recipient email and name
+
+// Set email body
+$email->addContent("text/plain", $email_body);
+
+$sendgrid = new \SendGrid('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
+'); // Set SendGrid API key
+
+try {
+    $response = $sendgrid->send($email); // Send email
+    echo "Email sent successfully.";
+} catch (Exception $e) {
+    echo "Email not sent. Error message: ".$e->getMessage();
+}
+
+?>
